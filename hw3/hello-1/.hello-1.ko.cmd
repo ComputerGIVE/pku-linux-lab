321fa73f97e07f834e3f9f966a1cbd58cb517b2a
Li Yilong <gnoliyil@pku.edu.cn>
2015-10-05 23:38:15 +0800

    Built module hello-1 (hw3)

---
+++ hw3/hello-1/.hello-1.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/gnoliyil/osprac/hw3/hello-1/hello-1.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/gnoliyil/osprac/hw3/hello-1/hello-1.ko /home/gnoliyil/osprac/hw3/hello-1/hello-1.o /home/gnoliyil/osprac/hw3/hello-1/hello-1.mod.o
---
+++ hw3/hello-1/.tmp_versions/hello-1.mod
@@ -0,0 +1,2 @@
+/home/gnoliyil/osprac/hw3/hello-1/hello-1.ko
+/home/gnoliyil/osprac/hw3/hello-1/hello-1.o
